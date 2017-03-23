7b18bfceb26281cbe5f1d6cd56a1b9572c98bd82
Jay Ravaliya <jayrav13@gmail.com>
2017-03-23 13:35:39 -0400

    Initial commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Response;
+use Input;
+use App\Models\User;
+use Validator;
+use Hash;
+use App;
+
+class UsersController extends Controller
+{
+
+	public function get(Request $request)
+	{
+		return $request->user();
+	}
+
+	public function create(Request $request)
+	{
+
+		if($this->validateUser(Input::all()))
+		{
+			$user = new User;
+			$user->fill(Input::all());
+			$user->password = Hash::make(Input::get('password'));
+			$user->api_token = str_random(60);
+			$user->save();
+			return Response::json($user, 200);
+		}
+		else
+		{
+			App::abort(500);
+		}
+
+	}
+
+	/**
+	 *	validateUser
+	 *
+	 *	Confirms inputs for User.
+	 */
+	private function validateUser($input)
+	{
+		$validator = Validator::make($input, [
+			"name" => "required",
+			"email" => "required|email|unique:users,email",
+			"password" => "required|min:8|max:16"
+		]);
+
+		if($validator->fails())
+		{
+			App::abort(400, $validator->errors());
+		}
+
+		return true;
+
+	}
+
+}
