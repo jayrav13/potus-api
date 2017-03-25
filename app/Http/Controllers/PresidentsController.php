cdf3573525bf2ac11758ee23d5ae6dc233838b6e
Jay Ravaliya <jayrav13@gmail.com>
2017-03-24 22:27:00 -0400

    Cleaned up all controllers, added relationship functionality

--- app/Http/Controllers/PresidentsController.php
+++ app/Http/Controllers/PresidentsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\President;
+use App\Models\VicePresident;
 use Response;
 
 class PresidentsController extends Controller
@@ -12,7 +13,26 @@ class PresidentsController extends Controller
 	public function index(Request $request)
 	{
 		$presidents = President::all();
+
+		for($i = 0; $i < count($presidents); $i++)
+		{
+			$presidents[$i] = $this->get_vice_presidents($presidents[$i]);
+		}
+
 		return Response::json($presidents, 200);
+	}
+
+	private function get_vice_presidents($president)
+	{
+		if($president->end_date != null)
+		{
+			$president->vice_presidents = VicePresident::where('start_date', '<=', $president->end_date)->where('end_date', '>=', $president->start_date)->where('start_date', '!=', $president->end_date)->where('end_date', '!=', $president->start_date)->get();
+		}
+		else
+		{
+			$president->vice_presidents = VicePresident::where('end_date', null)->get();
+		}
+		return $president;
 	}
 
 }
--- app/Http/Controllers/VicePresidentsController.php
+++ app/Http/Controllers/VicePresidentsController.php
@@ -16,17 +16,23 @@ class VicePresidentsController extends Controller
 
 		for($i = 0; $i < count($vice_presidents); $i++)
 		{
-			if($vice_presidents[$i]->end_date != null)
-			{
-				$vice_presidents[$i]->presidents = President::where('start_date', '<=', $vice_presidents[$i]->end_date)->where('end_date', '>=', $vice_presidents[$i]->start_date)->where('start_date', '!=', $vice_presidents[$i]->end_date)->where('end_date', '!=', $vice_presidents[$i]->start_date)->get();
-			}
-			else
-			{
-				$vice_presidents[$i]->presidents = President::where('end_date', null)->get();
-			}
+			$vice_presidents[$i] = $this->get_presidents($vice_presidents[$i]);
 		}
 
 		return Response::json($vice_presidents, 200);
+	}
+
+	private function get_presidents($vice_president)
+	{
+		if($vice_president->end_date != null)
+		{
+			$vice_president->presidents = President::where('start_date', '<=', $vice_president->end_date)->where('end_date', '>=', $vice_president->start_date)->where('start_date', '!=', $vice_president->end_date)->where('end_date', '!=', $vice_president->start_date)->get();
+		}
+		else
+		{
+			$vice_president->presidents = President::where('end_date', null)->get();
+		}
+		return $vice_president;
 	}
 
 }
